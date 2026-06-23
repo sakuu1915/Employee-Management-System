b6942d2466a81457b786e2ce755dcc10be11d4e7
sakuu1915 <sakshihavaldar221@gmail.com>
2026-06-23 18:42:01 +0530

    Updated API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://employee-management-system-cg1b.onrender.com",
+  baseURL: "https://employee-management-system-cg1b.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
